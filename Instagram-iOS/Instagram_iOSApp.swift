b94f70d031eb095fe73669e01af8a4a833ba6ba3
fasoxy-it <fasoxy@gmail.com>
2021-04-12 22:01:24 +0200

    Initial Commit

---
+++ Instagram-iOS/Instagram_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_iOSApp.swift
+//  Instagram-iOS
+//
+//  Created by Mattia Fasoli on 12/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
